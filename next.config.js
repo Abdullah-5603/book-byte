61972e39b6e720764fd23f4bbf76b28d52e2880b
MD Abu Abdullah <abdullah72308@gmail.com>
2025-11-26 10:22:57 +0600

    next.js config updated

--- next.config.js
+++ next.config.js
@@ -1,12 +1,34 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     // loader: 'imgix', // or other loaders if preferred
+//     // path: 'https://example.com/_next/image',
+//     domains: ['images-na.ssl-images-amazon.com', 'i.scdn.co', "i.gr-assets.com"],
+//     // New 'remotePatterns' to specify allowed remote patterns
+//     // remotePatterns: ['https:/images-na.ssl-images-amazon.com/*', 'https://i.scdn.co/*', "https://i.gr-assets.com/*"],
+//     },
+// }
+
+// module.exports = nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    // loader: 'imgix', // or other loaders if preferred
-    // path: 'https://example.com/_next/image',
-    domains: ['images-na.ssl-images-amazon.com', 'i.scdn.co', "i.gr-assets.com"],
-    // New 'remotePatterns' to specify allowed remote patterns
-    // remotePatterns: ['https:/images-na.ssl-images-amazon.com/*', 'https://i.scdn.co/*', "https://i.gr-assets.com/*"],
-    },
-}
+  reactStrictMode: true,
 
-module.exports = nextConfig
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**"
+      }
+    ],
+    domains: ['images-na.ssl-images-amazon.com', 'i.scdn.co', "i.gr-assets.com"],
+  },
+
+  // Important for SCSS + Tailwind
+  sassOptions: {
+    silenceDeprecations: ["legacy-js-api"]
+  }
+};
+
+export default nextConfig;
